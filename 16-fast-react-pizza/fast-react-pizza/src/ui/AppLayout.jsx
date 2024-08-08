7a1234e68f0730169a8909b7118e7efc84fb64d4
qingyangucb <qingyangucb@gmail.com>
2024-08-08 15:05:59 -0700

    so fun

---
+++ 16-fast-react-pizza/fast-react-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,25 @@
+import Header from "./Header";
+import CartOverView from "../features/cart/CartOverView";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverView />
+    </div>
+  );
+}
+
+export default AppLayout;
