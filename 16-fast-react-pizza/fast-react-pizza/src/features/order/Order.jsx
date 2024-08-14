71d342c89f28a86bf081c0e429d1e7cefb4d5111
qingyangucb <qingyangucb@gmail.com>
2024-08-14 15:02:54 -0700

    finish 16

--- 16-fast-react-pizza/fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/Order.jsx
@@ -8,6 +8,7 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
@@ -19,8 +20,6 @@ function Order() {
     },
     [fetcher],
   );
-
-  console.log(fetcher.data);
 
   const {
     id,
@@ -88,6 +87,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
