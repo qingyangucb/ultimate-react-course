22c018024f950afeb51414a5b0cfd61b54f69f0f
qingyangucb <qingyangucb@gmail.com>
2024-08-15 15:27:00 -0700

    stuff

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -1,24 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import DashBoard from "./pages/Dashboard"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashBoard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='dashboard' element={<Dashboard />} />
-        <Route path='bookings' element={<Dashboard />} />
-        <Route path='users' element={<Dashboard />} />
-        <Route path='settings' element={<Dashboard />} />
-        <Route path='account' element={<Dashboard />} />
-        <Route path='login' element={<Dashboard />} />
-        <Route path='*' element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bookings" element={<Dashboard />} />
+        <Route path="users" element={<Dashboard />} />
+        <Route path="settings" element={<Dashboard />} />
+        <Route path="account" element={<Dashboard />} />
+        <Route path="login" element={<Dashboard />} />
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-expo<BrowserRrBrowserRouteruter>
-  <Routes>
-    <Route path='dashboard'  />
-  </Routes>
-</BrowserRruter>
+export default App;
