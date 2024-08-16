9d5d1a4dcaa4a7f29596b8c03e3f4d5aa7459bce
qingyangucb <qingyangucb@gmail.com>
2024-08-15 21:17:46 -0700

    fun

--- 17-the-wild-oasis/the-wild-oasis/src/ui/AppLayout.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 import styled from "styled-components";
 
 const Main = styled.main`
-  background-color: green;
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
 
--- 17-the-wild-oasis/the-wild-oasis/src/ui/Header.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/Header.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
-  background-color: orangered;
+  background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border: 1 solid var(--color-grey-100);
 `;
