aebbd4e29e25cef99118a94e72f1ebc38f98e3f9
qingyangucb <qingyangucb@gmail.com>
2024-08-25 19:52:38 -0700

    fun

--- 17-the-wild-oasis/the-wild-oasis/src/ui/Modal.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/Modal.jsx
@@ -3,6 +3,7 @@ import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutSideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -74,10 +75,12 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutSideClick(close);
+
   if (name !== openName) return null;
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
